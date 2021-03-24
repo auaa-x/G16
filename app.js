63407e2b371fbde76ce6d3108c11754ed9442182
mess <ren1010295282+github@gmail.com>
2021-03-24 08:49:25 +0800

    feat: add service worker

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/api');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
