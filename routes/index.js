fa324adfeeee5ea47295c74e0b406c22b8e27ba3
Ray494997 <76392591+Ray494997@users.noreply.github.com>
2021-05-19 13:50:07 +0100

    Fix: Chat UI

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Browsing' });
+  res.render('index', { title: 'My Chat' });
 });
-
 
 module.exports = router;
--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,3 +1,7 @@
+const n = require('../socket.io/dbutils');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://42.192.40.225:27017/newbegin";
+
 
 exports.init = function(io) {
 
@@ -9,19 +13,58 @@ exports.init = function(io) {
       /**
        * it creates or joins a room
        */
-      socket.on('create or join', function (room, userId) {
+      socket.on('create or join', function (room, userId , picUrl) {
         socket.join(room);
         chat.to(room).emit('joined', room, userId);
+
+        MongoClient.connect(url,function(err1,db){
+          if(err1) throw err1;
+          var dbo = db.db("newbegin");
+          dbo.collection("test").find({"_id":room}).toArray(function(err2,re1){
+              if(err2) throw err2;
+              if(re1.length!=0){
+                var map = new Map(Object.entries(re1[0]));
+                var arr1 = Object.values(map.get("chat"));
+                var arr2 = Object.values(map.get("draw"));
+                var pUrl = map.get("pic").toString();
+                setTimeout(() => {
+                    chat.to(room).emit('chatInit',room,userId,arr1);
+                    chat.to(room).emit('drawingInit',arr2);
+                    chat.to(room).emit('getPic',pUrl);
+                    //Pass an anothor url
+                }, 500);
+
+              }
+          })
+        })
+
+        setTimeout(() => {
+           n.insert_urlandRoom(picUrl,room);
+        }, 300);
+
       });
 
       socket.on('chat', function (room, userId, chatText) {
+        console.log("User"+userId);
         chat.to(room).emit('chat', room, userId, chatText);
+        //User name;
+        n.insert_chat(room,userId,chatText);
       });
 
       socket.on('disconnect', function(){
         console.log('someone disconnected');
       });
-      socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+
+      socket.on('point', (data,room) =>{
+        //console.log("data:"+data);
+        chat.to(room).emit('drawing', data);
+        n.insert_coordinate(data);
+      });
+
+      socket.on('delPos',(roomId)=>{
+        chat.to(roomId).emit('update point');
+        n.delete_pic(roomId);
+      })
     } catch (e) {
     }
   });
