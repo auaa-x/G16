49828b892369f4797f800b1fe5f6e219e63922a1
Ray494997 <76392591+Ray494997@users.noreply.github.com>
2021-05-19 13:43:44 +0100

    Update app.js
    Fix: Change router

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -23,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
