1a131aac442007633f27a4f6b415e9605dd2e443
ting <tguo8@sheffield.ac.uk>
2021-06-04 11:31:42 +0100

    feat: the update button of image information panel is now able to update related data in mongo db as well

--- controllers/users.js
+++ controllers/users.js
@@ -11,9 +11,9 @@ function insert(req, res) {
             user_name: userData.username,
             room_id: userData.roomNo,
             image_path: userData.imageUrl,
-            // image_title: '',
-            // image_author: '',
-            // image_dp: ''
+            image_title: ' ',
+            image_author: ' ',
+            image_dp: ' '
         });
         console.log('received: ' + user);
 
@@ -53,20 +53,31 @@ exports.checkExist = (req, res) => {
         });
 }
 
-// exports.updatePanel = (req, res) => {
-//     console.log("Updating image info panel...");
-//     let panelData = req.body;
-//     console.log(panelData);
-//
-//     User.updateOne( {
-//         room_id: panelData.roomNo,
-//         image_path: panelData.imageUrl
-//     }, {
-//         $set: {
-//             image_title: panelData.imageTitle,
-//             image_author: panelData.imageAuthor,
-//             image_dp: panelData.imageDp,
-//         }
-//         });
-// }
-//
+exports.updatePanel = (req, res) => {
+    console.log("Updating image info to mongo db...");
+    let panelData = req.body;
+    console.log(panelData);
+
+    User.updateOne({
+        room_id: panelData.roomNo,
+        image_path: panelData.imageUrl},
+        {
+            $set : {
+            image_title: panelData.imageTitle,
+            image_dp: panelData.imageDp,
+            image_author: panelData.imageAuthor
+            }}, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Mongo db successfully updated.");
+        }
+    });
+
+    // User.findOne({ 'room_id': panelData.roomNo}, 'image_path', function (err, user) {
+    //     if (err) return handleError(err);
+    //     // Prints "Space Ghost is a talk show host".
+    //     console.log('title: ' + user.image_path);
+    // });
+}
+
--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,6 @@ router.get('/', function(req, res, next) {
 
 router.post('/add', user.checkExist);
 
-// router.post('/updatePanel', user.updatePanel);
+router.post('/updatePanel', user.updatePanel);
 
 module.exports = router;
